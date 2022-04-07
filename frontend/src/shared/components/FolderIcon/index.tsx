885d372528db789b74a8f829edd224c4fd0e8502
Andrey Deriabin <aderiabin.98@gmail.com>
2022-04-08 01:32:16 +0300

    Frontend: added more folder icons

--- frontend/src/shared/components/FolderIcon/index.tsx
+++ frontend/src/shared/components/FolderIcon/index.tsx
@@ -1,25 +1,44 @@
 import React from 'react';
 import {
-    IoAddCircleOutline, IoAddCircleSharp,
-    IoSettingsOutline, IoSettingsSharp,
-    IoCheckmark, IoCheckmarkSharp,
-    IoCloudyOutline, IoCloudySharp,
-    IoCode, IoCodeSharp,
-    IoCubeOutline, IoCubeSharp,
-    IoHomeOutline, IoHomeSharp,
-    IoStarOutline, IoStarSharp,
+    IoAddCircleOutline,
+    IoAddCircleSharp,
+    IoSettingsOutline,
+    IoSettingsSharp,
+    IoCheckmark,
+    IoCheckmarkSharp,
+    IoCloudyOutline,
+    IoCloudySharp,
+    IoCode,
+    IoCodeSharp,
+    IoCubeOutline,
+    IoCubeSharp,
+    IoHomeOutline,
+    IoHomeSharp,
+    IoStarOutline,
+    IoStarSharp,
+    IoAirplaneSharp,
+    IoAirplaneOutline,
+    IoBarbellSharp,
+    IoBarbellOutline,
+    IoBriefcaseSharp,
+    IoBriefcaseOutline, IoFishSharp, IoFishOutline, IoRocketOutline, IoRocketSharp,
 } from 'react-icons/io5'
 import styles from '../../../NotesListView/SidePanel/styles.module.scss'
 import {IconType} from "react-icons";
 
-export type FolderIconNameType = 'add' | 'settings' | 'check' | 'cloud' |
-    'code' | 'cube' | 'home' | 'star' | string
+export type FolderIconNameType = 'add' | 'airplane' | 'barbell' | 'case'  |
+    'check' | 'cloud' | 'code' | 'cube' | 'fish' | 'home' | 'rocket' | 'star' | 'settings' | string
 export const UserFolderIcons = [
+    { id: 'airplane', title: 'Airplane' },
+    { id: 'barbell', title: 'Barbell' },
+    { id: 'case', title: 'Case' },
     { id: 'check', title: 'Checkmark' },
     { id: 'cloud', title: 'Cloud' },
     { id: 'code', title: 'Code' },
     { id: 'cube', title: 'Cube' },
-    { id: 'star', title: 'Star' }
+    { id: 'fish', title: 'Fish' },
+    { id: 'rocket', title: 'Rocket' },
+    { id: 'star', title: 'Star' },
 ]
 
 type FolderIconProps = {
@@ -33,8 +52,14 @@ function getIcon(props: FolderIconProps): IconType {
     switch (props.name) {
         case 'add':
             return isSel ? IoAddCircleSharp : IoAddCircleOutline
+        case 'airplane':
+            return isSel ? IoAirplaneSharp : IoAirplaneOutline
+        case 'barbell':
+            return isSel ? IoBarbellSharp : IoBarbellOutline
         case 'settings':
             return isSel ? IoSettingsSharp : IoSettingsOutline
+        case 'case':
+            return isSel ? IoBriefcaseSharp : IoBriefcaseOutline
         case 'check':
             return isSel ? IoCheckmarkSharp : IoCheckmark
         case 'cloud':
@@ -43,8 +68,12 @@ function getIcon(props: FolderIconProps): IconType {
             return isSel ? IoCodeSharp : IoCode
         case 'cube':
             return isSel ? IoCubeSharp : IoCubeOutline
+        case 'fish':
+            return isSel ? IoFishSharp : IoFishOutline
         case 'home':
             return isSel ? IoHomeSharp : IoHomeOutline
+        case 'rocket':
+            return isSel ? IoRocketSharp : IoRocketOutline
         case 'star':
             return isSel ? IoStarSharp : IoStarOutline
         default:
