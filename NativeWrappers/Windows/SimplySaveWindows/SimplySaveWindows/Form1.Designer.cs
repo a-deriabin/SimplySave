8c8ecbad59ec0aa8c90561221e06d265463011bd
Andrey Deriabin <aderiabin.98@gmail.com>
2022-04-08 18:35:11 +0300

    Windows: app logo

--- NativeWrappers/Windows/SimplySaveWindows/SimplySaveWindows/Form1.Designer.cs
+++ NativeWrappers/Windows/SimplySaveWindows/SimplySaveWindows/Form1.Designer.cs
@@ -29,6 +29,7 @@ namespace SimplySaveWindows
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.webView = new Microsoft.Web.WebView2.WinForms.WebView2();
             ((System.ComponentModel.ISupportInitialize)(this.webView)).BeginInit();
             this.SuspendLayout();
@@ -52,6 +53,7 @@ namespace SimplySaveWindows
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.webView);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "SimplySave";
             this.Load += new System.EventHandler(this.Form1_Load);
